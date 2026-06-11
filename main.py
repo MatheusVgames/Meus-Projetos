73bf3786b7cc587fe60cde3397fb6c48232a8455
MatheusVgames <matheuspvs956@gmail.com>
2026-06-11 10:27:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-
-print("Hello World")
+meme_dict = {
+            "CRINGE": "Algo vergonhoso ou constrangedor",
+            "STALKEAR": "Investigar a vida de alguém online",
+            "BUGOU": "Algo no pc travou",
+            "MANO": "Você / ele, Tom informal entre pessoas do mesmo nivel"
+            }
+word = input("Digite uma palavra moderna que você não entende):").upper()
+if word in meme_dict.keys():
+    print(meme_dict[word])
+    # O que devemos fazer se a palavra for encontrada?
+else:
+    print("infelismente não temos o significado para esse meme")
+    # O que devemos fazer se a palavra não for encontrada?
