89a6a783631467730a1d87f2522656c215cc49de
MatheusVgames <matheuspvs956@gmail.com>
2026-06-18 10:19:41 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-meme_dict = {
-            "CRINGE": "Algo vergonhoso ou constrangedor",
-            "STALKEAR": "Investigar a vida de alguém online",
-            "BUGOU": "Algo no pc travou",
-            "MANO": "Você / ele, Tom informal entre pessoas do mesmo nivel"
-            }
-word = input("Digite uma palavra moderna que você não entende):").upper()
-if word in meme_dict.keys():
-    print(meme_dict[word])
-    # O que devemos fazer se a palavra for encontrada?
-else:
-    print("infelismente não temos o significado para esse meme")
-    # O que devemos fazer se a palavra não for encontrada?
+import random
+
+caracteres =  "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+Numero_de_caracteres = input("digite o numero de caracteres:")
+if Numero_de_caracteres.isdigit():
+    Numero_de_caracteres = int(Numero_de_caracteres)
+    senha = ""
+    for i in range(Numero_de_caracteres):
+        senha += random.choice(caracteres)
+    print("sua senha é:", senha)
